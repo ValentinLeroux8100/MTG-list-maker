5af963ee7b676b22ddcb276d9369bc11ae66fe2f
ValentinLeroux8100 <74792769+ValentinLeroux8100@users.noreply.github.com>
2023-05-10 21:46:41 +0200

    drag and drop card
    Add drag and drop for card, need to change logic to manage different panel types more correctly

--- src/CardElement.js
+++ src/CardElement.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import cardNumberBackground from "./Graphics/Card Number Background.svg";
+import { Draggable } from 'react-beautiful-dnd'
 
-function CardElement({color, children}) {
-  
-
-  return (
-    <div className='card'>
-        <div className='card-number'>
-            <img src={cardNumberBackground} alt="cardNumberBackground" className='card-number-background'/>
-            <div className='card-number-text'>3x</div>
-        </div>
-        <div className='card-back'>
-            <div className='card-mid'>
-                <div className='card-top'>Alloy Animist</div>
-            </div>
-        </div>
-    </div>
+function CardElement({color, id, index, children}) {
+  return (  
+    <Draggable draggableId={id} index={index}>
+    {provider => (
+      <div {... provider.draggableProps} ref = {provider.innerRef} {... provider.dragHandleProps} className='card'>
+          <div className='card-number'>
+              <img src={cardNumberBackground} alt="cardNumberBackground" className='card-number-background'/>
+              <div className='card-number-text'>3x</div>
+          </div>
+          <div className='card-back'>
+              <div className='card-mid'>
+                  <div className='card-top'>{children}</div>
+              </div>
+          </div>
+      </div>
+    )}
+    </Draggable>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,60 +2,8 @@ import {React, useState} from 'react';
 import PanelContainer from './PanelContainer';
 import ReactDOM from 'react-dom/client';
 import { DragDropContext } from 'react-beautiful-dnd'
-import './style.css'
-/*
-var selectedPanel;
-var diffPos;
+import './style.scss'
 
-function MovePanelStart(panel, posX){
-    selectedPanel = panel;
-    diffPos = posX - panel.current.getClientRects()[0].x
-
-    selectedPanel.current.style.position = "absolute";
-    selectedPanel.current.style.left = posX-diffPos+"px";
-    selectedPanel.current.style.visibility = "visible"
-
-    window.addEventListener('mousemove', MovePanel);
-    window.addEventListener('mouseup', MovePanelRelease);
-}
-
-function MovePanel(e){
-    selectedPanel.current.style.left = e.clientX-diffPos+"px";
-}
-
-function MovePanelRelease(e){
-    selectedPanel.current.style.left = e.clientX-diffPos+"px";
-    selectedPanel.current.style.position = "inherit";
-
-    window.removeEventListener('mousemove', MovePanel);
-    window.removeEventListener('mouseup', MovePanelRelease);
-
-}
-
-function Panel({setText, visibility, copyPanel, children}){
-    const panel = useRef();
-
-    function startDrag(e){
-        //setText((texts) => [... texts, children[1]]);
-        MovePanelStart(copyPanel, e.clientX);
-    }
-    
-}
-
-function Principal({copyPanel}){
-    const [texts, setText] = useState(["test 1", "test 2"]);
-
-    const listPanel = (texts.map((text, id) =>
-        <Panel key={id} setText={setText} copyPanel={copyPanel}> {text}</Panel>
-    ));
-    
-    return(
-        <main>
-            {listPanel}
-        </main>
-    )
- }
-*/
 
 function App(){
     const [data, setData] = useState({ 
